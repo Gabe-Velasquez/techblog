0916deb58446e5fdc1268de850711579bbb61ca6
Gabe <gabriel.velasquez0116@gmail.com>
2023-10-10 18:07:27 -0700

    started getting skeleton of handlebar templates coded and started on some of the js

---
+++ public/js/login.js
@@ -0,0 +1,40 @@
+const loginFormHandler = async (event)=>{
+    event.preventDefault();
+    // Create values from login form
+    const username= document.querySelector('#username-login').value.trim();
+    const password=document.querySelector('#password-login').value.trim();
+    if(username && password){
+        const response = await fetch("/api/users/login",{
+            method:"POST",  
+            body: JSON.stringify({username,password}), 
+            headers:{'Content-Type':'application/json'} 
+        });
+        if(response.ok) {
+            document.location.replace("//");
+        }else{
+            alert('Failed to log in');
+        }
+    }
+};
+
+const signupFormHandler = async (event)=>{
+    event.preventDefault();
+    const username= document.querySelector('#username-login').value.trim();
+    const password=document.querySelector('#password-login').value.trim(); 
+
+    if(username && password){
+        const response = await fetch("/api/users",{
+            method:"POST",  
+            body: JSON.stringify({username,password}), 
+            headers:{'Content-Type':'application/json'} 
+        });
+        if(response.ok) {
+            document.location.replace("//");
+        }else{
+            alert('Failed to log in');
+        }
+    }
+};
+
+document.querySelector(".login-form").addEventListener("submit", loginFormHandler);
+document.querySelector(".signup-form").addEventListener("submit", signupFormHandler);
---
+++ public/js/new-post.js
@@ -0,0 +1,19 @@
+const newPostFormHandler = async (event)=>{
+    event.preventDefault();
+    const title=document.querySelector('#post-title').value.trim();
+    const content=document.querySelector('#post-entry').value.trim();
+
+    const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({ title,entry}),
+        headers:{'Content-Type':'application/json'},
+    });
+
+    if (response.ok){
+        document.location.replace("/");
+    }else{
+        alert(response);
+    }
+};
+
+document.querySelector('#new-post-form').addEventListener('submit', newPostFormHandler);
