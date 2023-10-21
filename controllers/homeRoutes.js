d23544ca70d91c2e92dc327e0f377acf0a6a98e3
Gabe <gabriel.velasquez0116@gmail.com>
2023-10-21 10:51:09 -0700

    made homeroutes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,54 @@
+const router = require('express').Router();
+const { User, Comment, Post } = require('../../models');
+const withAuth = require('../utils/auth');
+
+// homepage for all posts
+router.post('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [User],
+        });
+        const posts = postData.map((project) => project.get({ plain: true }));
+        res.render('homepage', {posts, logged_in: req.session.logged_in});
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+// fetch one post
+router.get('/post/:id', withAuth, async (req, res) =>{
+try{
+    const postData=await Post.findOne({
+        where:{
+            id:req.params.id
+            },
+            include:[
+                {
+                    model:Comment,
+                    include:{
+                        model:User,
+                        attributes:['username'],
+                    },
+                },
+                {
+                    model:User,
+                    attributes: ['username'],
+                },
+            ],
+    });
+    if(!postData){
+        res.status(404).end();
+    }else{
+        const post = postData.get({plain:true});
+        res.render('post-by-id', {post, logged_in:req.session.logged_in});
+    }
+    } catch(err){
+        res.status(500).json(err);
+    }
+});
+
+// Route to login/signup
+router.get("/login", (req,res)=>{
+    res.render('login');
+});
+module.exports = router;
