4166ee9f11cd03082b78e812639aec0e3221fbbf
Gabe <gabriel.velasquez0116@gmail.com>
2023-10-21 10:36:10 -0700

    made dashboard route

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+const { User, Comment, Post } = require('../models');
+const withAuth = require('../utils/auth');
+
+// Gather all posts
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      where: {
+        user_id: req.session.user_id, // Finds the logged in users posts only
+      },
+      include: [
+        {
+          model: Comment,
+          include: {
+            model: User,
+            attributes: ['username'],
+          },
+        },
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+    const posts = postData.map((post) => post.get({ plain: true }));
+    res.render('dashboard', { posts, logged_in: req.session.logged_in });
+  } catch (err) {
+    res.redirect('/');
+  }
+});
+
+// Route for new post
+router.get('/new', (req,res)=>{
+    res.render('new-post');
+});
+
+//edits
+router.get("/edit/:id", withAuth, async(req,res)=> {
+    try{
+        const editPost = await Post.findByPk(req.params.id);
+        if(!editPost){
+            res.status(404).end();
+            return;
+        }else{
+            const post=editPost.get({plain:true});
+            res.render('edit-post', {post});
+        }
+    }catch(err){
+        res.redirect('login');
+    }
+});
+module.exports = router;
